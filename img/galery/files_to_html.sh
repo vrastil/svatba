28ace7a108d64f578114a9ccccd2ffedc12fa7ed
vrastil <michalvrastil@seznam.cz>
2020-04-30 18:02:52 +0200

    create thumbmail

--- img/galery/files_to_html.sh
+++ img/galery/files_to_html.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-FILES=$(ls *.{jpg,jpeg,.png} 2>/dev/null)
+# delete old files
+THUMB_FILES=$(ls thumb*.{jpg,jpeg,.png} 2>/dev/null)
 HTML_FILE=img.html
+THUMB_HTML_FILE=thumb.html
+
 echo "" > $HTML_FILE
+echo "" > $THUMB_HTML_FILE
+rm $THUMB_FILES
+
+# write new files
+FILES=$(ls *.{jpg,jpeg,.png} 2>/dev/null)
 i=0
 for f in $FILES
 do
-  ID="img_$i"
-  echo "							<img class=\"item\" style=\"height:100%;max-height:250px;\" src=\"img/galery/$f\" alt=\"\">" >> $HTML_FILE
+  # create thumbnail
+  convert -thumbnail x200 $f thumb.$f
+  echo "<img class=\"item\" src=\"img/galery/$f\">" >> $HTML_FILE
+  echo "<img class=\"item\" src=\"img/galery/thumb.$f\">" >> $THUMB_HTML_FILE
   i=i+1
 done
